01d47815562d4ec16025145127690f1326582cb9
Shankhwar <91554563+Shankhwar@users.noreply.github.com>
2022-03-11 21:58:52 +0530

    added about page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 // import Footer from "./Componentes/Footer/Footer";
 // import Main from "./Componentes/Navbar/Main";
 
+import About from "./Componentes/About/About";
 import Intro from "./Componentes/Intro/Intro";
 
 function App() {
   return (
     <>
       <Intro />
+      <About />
       {/* <Main />
       <Footer /> */}
     </>
---
+++ src/Componentes/About/About.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./About.css";
+
+function About() {
+  return (
+    <div className="a">
+      <div className="a-left">
+        <div className="a-card bg"> </div>
+        <div className="a-card">
+          <img
+            src="https://www.somagnews.com/wp-content/uploads/2020/06/38-10-e1592081237169.jpg"
+            alt=""
+            className="a-img"
+          />
+        </div>
+      </div>
+      <div className="a-right"></div>
+    </div>
+  );
+}
+
+export default About;
